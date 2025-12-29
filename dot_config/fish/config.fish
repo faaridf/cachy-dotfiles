fd567b3e1d67d6b3a8232433ec0cf8dd59c0d291
Faarid <faaridfarooq06@gmail.com>
2025-12-29 17:00:35 +0500

    Update .chezmoiignore Update .config/fish/config.fish Update .config/fish/fish_variables Update .config/kitty/kitty.conf

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -8,3 +8,4 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 zoxide init fish | source
 set -gx EDITOR nvim
+oh-my-posh init fish --config "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/heads/main/themes/1_shell.omp.json" | source
